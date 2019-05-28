34ebe343c5dee8dfe845448e90217cefd3d64e0c
Karan Kumar <karan.kumar@bainslabs.com>
2019-05-28 17:42:13 +0530

    whatsapp adition

--- sever.js
+++ sever.js
@@ -2,9 +2,12 @@ const accountSid = "ACe7052284e7ac8ba78bd33d26189ae3ca";
 const authToken = "dba0ca48b08de8714cc022d56d578041";
 const client = require("twilio")(accountSid, authToken);
 
-const numbers = ["+919878335575", "+919876830251"];
-numbers.forEach(number => {
-  client.messages
-    .create({ from: "+18326788823", body: "testing twilio", to: number })
-    .then(message => console.log(message.sid));
-});
+// const numbers = ["+919878335575", "+919876830251"];
+
+client.messages
+  .create({
+    from: "whatsapp:+14155238886",
+    body: "testing twilio",
+    to: "whatsapp:+919878335575"
+  })
+  .then(message => console.log(message.sid));
